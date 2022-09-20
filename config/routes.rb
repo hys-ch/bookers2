b9a5999b01d732bef9c4a29578f070cb22ec5397
hys-ch <108731664+hys-ch@users.noreply.github.com>
2022-09-20 21:43:39 +0000

    〔Add〕User info

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :index, :show]
   resources :books, only: [:new, :index, :show, :edit]
 
+  post "books" => "books#create"
+
   root to: "homes#top"
   get "/home/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
