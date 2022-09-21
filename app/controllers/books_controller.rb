1dea861a511f71c19050d40f56ec2621a6b28d19
hys-ch <108731664+hys-ch@users.noreply.github.com>
2022-09-21 08:10:24 +0000

    〔Add〕Bookの一覧ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,15 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
@@ -11,4 +17,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
