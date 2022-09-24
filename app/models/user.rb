75ad7704d115dc97f2a29eac90e75d2c332a5b9b
hys-ch <108731664+hys-ch@users.noreply.github.com>
2022-09-24 09:54:06 +0000

    〔Update〕バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,10 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
+  validates :name,
+    length: { minimum: 2, maximum: 20},presence: true
+
+  validates :introduction,
+    length: {maximum: 50},presence: true
+
 end
